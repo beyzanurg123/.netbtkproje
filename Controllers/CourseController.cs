84c100aef87ef151abb113d874eba25bc096d06e
Beyzanur Gündoğdu <190403045@ogr.gelisim.edu.tr>
2023-09-06 08:39:01 +0300

    first commit

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,32 @@
+using btk.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace btk.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model=Repository.Applications;
+            return View(model);
+        }
+         public IActionResult Apply()
+        {
+            return View();
+        }
+
+[HttpPost]    
+[ValidateAntiForgeryToken]
+    public IActionResult Apply([FromForm]Candidate model)
+        {
+            if(Repository.Applications.Any(c=> c.Email.Equals(model.Email))){
+                ModelState.AddModelError("","There is already application");
+            }
+            if(ModelState.IsValid){
+   Repository.Add(model);
+            return View("Feedback",model);
+            }
+         return View();
+        }
+    }
+}
